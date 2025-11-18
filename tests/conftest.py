a504c329d7305049ba51acebb29e34c44be99f56
pascales dedy kurniawan <pascalesdedy@pascaless-MacBook-Pro.local>
2025-11-18 15:39:34 +0700

    adding reports and screenshots on failed testcase

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from utils.driver_factory import get_driver
+from datetime import datetime
 
 @pytest.fixture
 def driver(request):
@@ -11,3 +12,24 @@ def driver(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
+
+def pytest_configure(config):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    config.option.htmlpath = f"reports/report_{timestamp}.html"
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            screenshot = f"screenshots/{item.name}.png"
+            driver.save_screenshot(screenshot)
+
+            # Attach to HTML report
+            if hasattr(item.config, "_html"):
+                extra = getattr(item.config, "extra", [])
+                extra.append(pytest_html.extras.png(screenshot))
+                item.config._html.extras = extra
